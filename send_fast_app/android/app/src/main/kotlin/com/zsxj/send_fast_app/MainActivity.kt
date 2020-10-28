5ad6cc09c87a9de0b6a9baa0d52d2c3335b86a54
wangyangjun <wangyangjun@huice.com>
2020-10-28 13:31:06 +0800

    no message

--- send_fast_app/android/app/src/main/kotlin/com/zsxj/send_fast_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.zsxj.send_fast_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ send_faster_app/android/app/src/main/java/com/zsxj/send_faster_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.zsxj.send_faster_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
